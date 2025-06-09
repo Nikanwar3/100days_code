904e4cc3074a38620a7a19ec794a303b14a4ea01
Nikanwar3 <nikanwar333@gmail.com>
2025-06-09 23:03:55 +0530

    largest number

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array{
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+
+        for(int i =0; i<numbers.length;i++){
+            if(largest <numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void  main(String[]args){
+        int array[] = {1,2,6,3,5};
+        System.out.println("largest valur is :" + getLargest(array));
+
+    }
+}
