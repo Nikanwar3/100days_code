2af3b904fefad9253865363b2a079a9c1fef572c
Nikanwar3 <nikanwar333@gmail.com>
2025-06-22 17:05:57 +0530

    variables n data types

---
+++ Basics/Basic.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Basic{
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        // float a = 4.4f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        // char a = 'a';
+        // char b = 'b';
+       
+        // System.err.println((int)a);
+        // System.err.println((int)b);
+        // System.out.println(a-b);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int N = (a+b+c)/3;
+        // System.out.println(N);
+
+        // int side = sc.nextInt();
+        // int area = side * side;
+        // System.out.println(area);
+
+
+        float pencil = 10.5f;
+        float pen = 20.5f;
+        float eraser = 5.5f;
+        int $ =24;
+        
+
+
+
+    }
+}
