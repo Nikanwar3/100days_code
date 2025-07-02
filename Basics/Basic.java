d520f100e6bc61b93749407c85044224877f3282
Nikanwar3 <nikanwar333@gmail.com>
2025-07-02 23:01:10 +0530

    linearSearch

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,7 +1,24 @@
-import java.util.*;
+// import java.util.*;
 public class Basic{
+
+
+
+    public static int getlargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i =0;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+
+
+    }
     public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
+
+        int numbers[] = {1,2,6,3,5};
+        System.out.println("largest number is : " + getlargest(numbers) );
+        // Scanner sc = new Scanner(System.in);
         // float a = 4.4f;
         // int b = (int)a;
         // System.out.println(b);
@@ -23,11 +40,12 @@ public class Basic{
         // System.out.println(area);
 
 
-        float pencil = 10.5f;
-        float pen = 20.5f;
-        float eraser = 5.5f;
-        int $ =24;
+        // float pencil = 10.5f;
+        // float pen = 20.5f;
+        // float eraser = 5.5f;
+        // int $ =24;
         
+
 
 
 
