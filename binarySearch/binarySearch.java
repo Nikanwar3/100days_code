5abcb8b818081e8aafbc17cd9ccec08a66f5a696
Nikanwar3 <nikanwar333@gmail.com>
2025-07-06 15:36:15 +0530

    binarySearch

---
+++ binarySearch/binarySearch.java
@@ -0,0 +1,31 @@
+public class binarySearch {
+    public static int binarySearch(int numbers[], int key) {
+        int start = 0, end = numbers.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (numbers[mid] == key) {
+                return mid;
+            } else if (numbers[mid] < key) {
+                start = mid + 1; // Search right half
+            } else {
+                end = mid - 1;   // Search left half
+            }
+        }
+        
+        return -1; // Key not found
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+
+        int result = binarySearch(numbers, key);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
