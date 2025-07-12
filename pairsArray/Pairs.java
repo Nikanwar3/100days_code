9b10a892973201392ef70d6e547e8e4b188d1317
Nikanwar3 <nikanwar333@gmail.com>
2025-07-12 19:01:23 +0530

    class

--- pairsArray/Pairs.java
+++ pairsArray/Pairs.java
@@ -1,38 +1,40 @@
 
-// public class Pairs {
+public class Pairs {
 
-//     public static void printPairs(int numbers[]) {
-//         for( int i =0;i<numbers.length;i++) {
-//             int curr = numbers[i];
-//             for(int j = i+1; j<numbers.length;j++) {
-//                 System.out.print("("+curr+","+numbers[j]+") ");
-//             }        
-//         System.out.println();
-//         }
-//     }
-//     public static void main(String[]args) {
-//         int numbers[] = {2,4,6,8,10};
-//         printPairs(numbers);
-//     }
-
-// }
-public class Pairs{
-
-    public static void  printPairs(int numbers[]) {
-        for(int i =0;i<numbers.length;i++) {
-           int curr = numbers[i];
-            for(int j = i+1;j<numbers.length;j++) {
-                System.out.println("("+curr+","+ numbers[j]+ ") ");
-
-            }
-            System.out.println();
-
+    public static void printPairs(int numbers[]) {
+        int tp =0;
+        for( int i =0;i<numbers.length;i++) {
+            int curr = numbers[i];
+            for(int j = i+1; j<numbers.length;j++) {
+                System.out.print("("+curr+","+numbers[j]+") ");
+            }        
+        System.out.println();
         }
     }
-
-
     public static void main(String[]args) {
         int numbers[] = {2,4,6,8,10};
         printPairs(numbers);
     }
+
 }
+// public class Pairs{
+
+//     public static void  printPairs(int numbers[]) {
+//         for(int i =0;i<numbers.length;i++) {
+//            int curr = numbers[i];
+//             for(int j = i+1;j<numbers.length;j++) {
+//                 System.out.println("("+curr+","+ numbers[j]+ ") ");
+
+//             }
+//             System.out.println();
+
+//         }
+//     }
+
+
+//     public static void main(String[]args) {
+//         int numbers[] = {2,4,6,8,10};
+//         printPairs(numbers);
+//     }
+// }
+
