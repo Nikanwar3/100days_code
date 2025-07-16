d7816db3b3c07e07f6d29744242bdab15ca64ffb
Nikanwar3 <nikanwar333@gmail.com>
2025-07-16 22:36:58 +0530

    binaryTree

---
+++ binaryTree/binaryTree.java
@@ -0,0 +1,26 @@
+
+public class binaryTree{
+
+
+    static class Node{
+        int data;
+        Node left = null;
+        Node right = null;
+        Node(int data){
+            this.data = data;
+            this.right = null;
+            this.left = null;
+
+        }
+
+        static class BinaryTree{
+             static int index = -1;
+             public static Node buildTree()
+        }
+    }
+
+
+    public static void main(String[]args){
+        int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    }
+}
