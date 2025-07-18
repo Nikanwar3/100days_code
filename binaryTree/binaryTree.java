54474cfad331f43ca33bf913b52511a2aca3765f
Nikanwar3 <nikanwar333@gmail.com>
2025-07-18 21:23:09 +0530

    linear

--- binaryTree/binaryTree.java
+++ binaryTree/binaryTree.java
@@ -19,6 +19,6 @@ public class binaryTree{
         }
     }
         public static void main(String[]args){ 
-        int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // 
     }
 }
