33b66ca80c638018103249045c75ed5cd72811b8
Nikanwar3 <nikanwar333@gmail.com>
2025-07-23 23:04:17 +0530

    binaryTree

--- binaryTree/binaryTree.java
+++ binaryTree/binaryTree.java
@@ -1,24 +1,57 @@
 
+// public class binaryTree{
+
+
+//     static class Node{
+//         int data;
+//         Node left = null;
+//         Node right = null;
+//         Node(int data){
+//             this.data = data;
+//             this.right = null;
+//             this.left = null;
+
+//         }
+
+//         static class BinaryTree{
+//              static int index = -1;
+//              public static Node buildTree(){
+//         }
+//     }
+//         public static void main(String[]args){ 
+//         int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // 
+//     }
+// }
 public class binaryTree{
 
 
     static class Node{
         int data;
-        Node left = null;
-        Node right = null;
+        Node right;
+        Node left;
         Node(int data){
             this.data = data;
             this.right = null;
             this.left = null;
-
-        }
-
-        static class BinaryTree{
-             static int index = -1;
-             public static Node buildTree(){
         }
     }
-        public static void main(String[]args){ 
-        int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // 
+    static class BinaryTree{
+        static int index = -1;
+        public static Node buildTree(int nodes[]){
+            index++;
+            if(nodes[index] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void main(String[]args){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        
+
     }
 }
