5bde93bfbd955286d34e918c3b0c847e901b1270
Nikanwar3 <nikanwar333@gmail.com>
2025-07-31 22:59:39 +0530

    priority

--- priority.java/Priority.java
+++ priority.java/Priority.java
@@ -1,64 +1,83 @@
 
-// import java.util.Comparator;
+// // import java.util.Comparator;
+// // import java.util.PriorityQueue;
+
+// // public class Priority{
+// //     public static void main(String[]args){
+// //         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+// //         pq.add(3); 
+// //         pq.add(2);
+// //         pq.add(1);   //O(log n)
+
+// //         while(! pq.isEmpty()){
+// //             System.out.println(pq.peek());   //O(1)
+// //             pq.remove();;   //O(log n)
+// //         }
+// //     }
+    
+// // }
+
+// // import java.util.PriorityQueue;;
+// // public class Priority{
+// //     static class Student implements Comparable<Student> {
+// //         String name;
+// //         String rank;
+
+// //         public Student(String name, int rank){      //constructor 
+// //             this.name = name;
+// //             this.rank = rank;
+// //         }
+// //     @Overrride
+        
+
+// //     }
+// // }
+
 // import java.util.PriorityQueue;
 
 // public class Priority{
-//     public static void main(String[]args){
-//         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-//         pq.add(3); 
-//         pq.add(2);
-//         pq.add(1);   //O(log n)
-
-//         while(! pq.isEmpty()){
-//             System.out.println(pq.peek());   //O(1)
-//             pq.remove();;   //O(log n)
-//         }
-//     }
-    
-// }
-
-// import java.util.PriorityQueue;;
-// public class Priority{
-//     static class Student implements Comparable<Student> {
+//     static class Student implements Comparable<Student>{
 //         String name;
-//         String rank;
+//         int rank;
 
-//         public Student(String name, int rank){      //constructor 
-//             this.name = name;
+//         public Student(String name, int rank){      //constructor
 //             this.rank = rank;
-//         }
-//     @Overrride
-        
+//             this.name = name;
 
+//         }
+//         @Override
+//         public int compareTo(Student s2){
+//             return this.rank - s2.rank;
+//         }
 //     }
 // }
 
 import java.util.PriorityQueue;
 
-public class Priority{
-    static class Student implements Comparable<Student>{
+public class Priority {
+    static class Student implements Comparable<Student> {
         String name;
         int rank;
 
-        public Student(String name, int rank){      //constructor
-            this.rank = rank;
+        public Student(String name, int rank) {
             this.name = name;
-
+            this.rank = rank;
         }
+
         @Override
-        public int compareTo(Student s2){
-            return this.rank - s2.rank;
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;  // Ascending order of rank
+        }
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 2));
+        pq.add(new Student("B", 1));
+
+        while (!pq.isEmpty()) {
+            Student s = pq.remove();
+            System.out.println(s.name + " " + s.rank);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
