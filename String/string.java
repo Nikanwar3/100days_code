d0a0977265b71334d9984e69d33eb1499b84151b
Nikanwar3 <nikanwar333@gmail.com>
2025-08-11 09:17:31 +0530

    String

---
+++ String/string.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class string{
+    public static void printLetters(String str){
+        for(int i =0; i<str.length();i++){
+            System.out.print(str.charAt(i) + "");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[]args){
+        // String str = "abcd";
+        // String str2 = new String("abcd");
+        // Scanner sc = new Scanner(System.in);
+        // String name ;
+        // name = sc.nextLine();
+        String name = "Nidhi";
+        String surname = "Kanwar";
+        String fullName = name + " " + surname;
+
+        // System.out.println(fullName);
+        // System.out.println(fullName.length());
+        // System.out.println(fullName.charAt(3));
+        printLetters(fullName);
+
+    }
+}
