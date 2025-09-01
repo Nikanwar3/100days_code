99c278f5113a0da1f4d97c1e9e4d8cbf013c750d
Nikanwar3 <nikanwar333@gmail.com>
2025-09-01 22:47:31 +0530

    sum of n natural

--- Basics/statement.java
+++ Basics/statement.java
@@ -1,13 +1,31 @@
-// we have to print 1  to n . n will deide by taking input from user
+// // we have to print 1  to n . n will deide by taking input from user
+// import java.util.*;
+// public class statement{
+//     public static void main(String[]args){
+//         int counter = 1;
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         while(counter<=n){
+//             System.out.println(counter + " ");
+//             counter++;
+//         }
+//         System.out.println();
+//     }
+// }
 import java.util.*;
-public class statement{
-    public static void main(String[]args){
-        int counter = 1;
+import java.util.Scanner;
+
+public class statement {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while(counter<=n){
-            System.out.println(counter + " ");
-            counter++;
+        int n = sc.nextInt();   
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            System.out.println(sum);  
+            sum = sum + i;            
+            i++;                     
         }
         System.out.println();
     }
