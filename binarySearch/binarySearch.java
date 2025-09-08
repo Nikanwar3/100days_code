b27875d2d8938b606b6e6fcf0e57f35a9efecaec
Nikanwar3 <nikanwar333@gmail.com>
2025-09-08 23:14:50 +0530

    binarySearch

--- binarySearch/binarySearch.java
+++ binarySearch/binarySearch.java
@@ -1,31 +1,62 @@
-public class binarySearch {
-    public static int binarySearch(int numbers[], int key) {
-        int start = 0, end = numbers.length - 1;
+// public class binarySearch {
+//     public static int binarySearch(int numbers[], int key) {
+//         int start = 0, end = numbers.length - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
+//         while (start <= end) {
+//             int mid = (start + end) / 2;
             
-            if (numbers[mid] == key) {
-                return mid;
-            } else if (numbers[mid] < key) {
-                start = mid + 1; // Search right half
-            } else {
-                end = mid - 1;   // Search left half
-            }
-        }
+//             if (numbers[mid] == key) {
+//                 return mid;
+//             } else if (numbers[mid] < key) {
+//                 start = mid + 1; // Search right half
+//             } else {
+//                 end = mid - 1;   // Search left half
+//             }
+//         }
         
-        return -1; // Key not found
-    }
+//         return -1; // Key not found
+//     }
 
-    public static void main(String[] args) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14};
-        int key = 10;
+//     public static void main(String[] args) {
+//         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+//         int key = 10;
 
-        int result = binarySearch(numbers, key);
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found"); // Output if key is not found
+//         int result = binarySearch(numbers, key);
+//         if (result != -1) {
+//             System.out.println("Element found at index: " + result);
+//         } else {
+//             System.out.println("Element not found"); // Output if key is not found
+//         }
+//     }
+// }
+
+public class binarySearch{
+    public  static int binary(int numbers[],int key){
+        int start = 0, end = numbers.length -1 ;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(numbers[mid]== key){
+                return mid;
+            }
+            else if (numbers[mid] < key){//right
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+
+            }
+            return -1;
+
+            }
+        
+        public static void main(String[]args){
+            int numbers[] = {2,4,6,8,10};
+            int key = 6;
         }
-    }
+        System.out.println(binary(numbers,key));
 }
+}
+    
+
+    
