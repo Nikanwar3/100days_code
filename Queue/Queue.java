8c5c76532d26b582e9f9d15a9ca9f9f125adc90e
Nikanwar3 <nikanwar333@gmail.com>
2025-09-11 21:37:04 +0530

    Meaningful commit message

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,26 +1,87 @@
+// public class Queue {
+//     static class Queue {
+//         static int arr[];
+//         static int size;
+//         Queue (int n ) {
+//             arr = new int[n];
+//             this.size = n;
+//         }
+//         public static boolean isEmpty(){
+//             return rear == -1;
+//         }
+//         public static void add(int data) {
+//             if(rear == size-1){
+//                 System.out.println("full queue");
+//                 return;
+//             }
+//             rear ++;
+//             arr[rear] = data;
+// // rear
+//         }
+
+//         public static void main(string[]args) {
+
+//         }
+//     }
+// }
+
+// public class Queue {
+//     static class Queue {
+//         static int arr[];
+//         static int size;
+//         Queue (int n ) {
+//             arr = new int[n];
+//             this.size = n;
+//         }
+//         public static boolean isEmpty(){
+//             return rear == -1;
+//         }
+//         public static void add(int data) {
+//             if(rear == size-1){
+//                 System.out.println("full queue");
+//                 return;
+//             }
+//             rear ++;
+//             arr[rear] = data;
+// // rear
+//         }
+
+//         public static void main(string[]args) {
+
+//         }
+//     }
+// }
+
+
 public class Queue {
-    static class Queue {
-        static int arr[];
-        static int size;
-        Queue (int n ) {
-            arr = new int[n];
-            this.size = n;
-        }
-        public static boolean isEmpty(){
-            return rear == -1;
-        }
-        public static void add(int data) {
-            if(rear == size-1){
-                System.out.println("full queue");
-                return;
-            }
-            rear ++;
-            arr[rear] = data;
-// rear
-        }
+    static int arr[];
+    static int size;
+    static int rear = -1;
+    static int front = -1;
 
-        public static void main(string[]args) {
+    Queue(int n) {
+        arr = new int[n];
+        size = n;
+    }
 
+    public static boolean isEmpty() {
+        return rear == -1;
+    }
+
+    public static void add(int data) {
+        if (rear == size - 1) {
+            System.out.println("Queue full");
+            return;
         }
+        rear++;
+        arr[rear] = data;
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(10);
+        q.add(20);
+        System.out.println(q.isEmpty());
     }
 }
+
