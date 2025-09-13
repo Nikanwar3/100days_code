5ec42f4f012fb0d8f7581da32541f077617dc89b
Nikanwar3 <nikanwar333@gmail.com>
2025-09-13 21:57:40 +0530

    max sumarray

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,44 +1,24 @@
-import java.util.*;
-public class Basic {
+// 
 
-    public static int getSmallest(int numbers[]) {
-        int smallest = Integer.MAX_VALUE;
-        for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i] < smallest) {  // Fixed this condition
-                smallest = numbers[i];
+public class Basic{
+    public static void subArray(int numbers[]){
+        int currSum =0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i =0; i<numbers.length;i++){
+            int start = i;
+            for (int j =i; j<numbers.length;j++){
+                int end = j;
+                currSum = 0;
+                for(int k = start; k<=end;k++){
+                    currSum += numbers[k];
+
+                }
+                if (maxSum< currSum){
+                    maxSum= currSum;
+                }
+                }
             }
         }
-        return smallest;
-    }  // Added this missing closing brace
-
-    public static void main(String[] args) {
-        int numbers[] = {1, 2, 6, 3, 5};
-        System.out.println("smallest number is : " + getSmallest(numbers));
-        
-        Scanner sc = new Scanner(System.in);
-        float a = 4.4f;
-        int b = (int)a;
-        System.out.println(b);
-
-        char a = 'a'; // character 'char' cannot be redeclared in the same scope
-        char b = 'b';// character ,,
-       
-        // System.err.println((int)a);
-        // System.err.println((int)b);
-        // System.out.println(a-b);
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
-        // int c = sc.nextInt();
-        // int N = (a+b+c)/3;
-        // System.out.println(N);
-
-        // int side = sc.nextInt();
-        // int area = side * side;
-        // System.out.println(area); print
-
-        // float pencil = 10.5f; // float
-        // float pen = 20.5f; // float
-        // float eraser = 5.5f;//float
-        // int $ =24;
+        System.out.println("Maximum SubArray Sum is: " + maxSum);
     }
-} //
+}
