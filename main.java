45bcf98a84d993ced6a214642ae1d4ac91a18d8f
Nikanwar3 <nikanwar333@gmail.com>
2025-09-24 21:13:46 +0530

    Add MedianFinder implementation

--- main.java
+++ main.java
@@ -1,8 +1,30 @@
-public class main{
-    public  static void main(String[]args){
-        System.out.println("*\n");
-        System.out.println("Hello World\n");
-        System.out.println("*\n");
-        System.out.println("Hello World!");
+// public class main{
+//     public  static void main(String[]args){
+//         System.out.println("*\n");
+//         System.out.println("Hello World\n");
+//         System.out.println("*\n");
+//         System.out.println("Hello World!");
+//     }
+// }
+
+import java.util.*;
+
+class MedianFinder {
+    private PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> right = new PriorityQueue<>();
+
+    public void addNum(int num) {
+        if (left.isEmpty() || num <= left.peek()) left.offer(num);
+        else right.offer(num);
+
+        if (left.size() > right.size() + 1) right.offer(left.poll());
+        if (right.size() > left.size()) left.offer(right.poll());
+    }
+
+    public double findMedian() {
+        if (left.size() == right.size())
+            return (left.peek() + right.peek()) / 2.0;
+        else
+            return left.peek();
     }
 }
