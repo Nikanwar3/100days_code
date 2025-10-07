e888b1e31aa284fbf0e03e0069ee11291ead190f
Nikanwar3 <nikanwar333@gmail.com>
2025-10-07 22:10:51 +0530

    Refactor Pairs code to use Java Streams and generics

--- pairsArray/Pairs.java
+++ pairsArray/Pairs.java
@@ -1,40 +1,61 @@
 
-public class Pairs {
+// public class Pairs {
 
-    public static void printPairs(int numbers[]) {
-        int tp =0;
-        for( int i =0;i<numbers.length;i++) {
-            int curr = numbers[i];
-            for(int j = i+1; j<numbers.length;j++) {
-                System.out.print("("+curr+","+numbers[j]+") ");
-            }        
-        System.out.println();
-        }
-    }
-    public static void main(String[]args) {
-        int numbers[] = {2,4,6,8,10};
-        printPairs(numbers);
-    }
-
-}
-// public class Pairs{
-
-//     public static void  printPairs(int numbers[]) {
-//         for(int i =0;i<numbers.length;i++) {
-//            int curr = numbers[i];
-//             for(int j = i+1;j<numbers.length;j++) {
-//                 System.out.println("("+curr+","+ numbers[j]+ ") ");
-
-//             }
-//             System.out.println();
-
+//     public static void printPairs(int numbers[]) {
+//         int tp =0;
+//         for( int i =0;i<numbers.length;i++) {
+//             int curr = numbers[i];
+//             for(int j = i+1; j<numbers.length;j++) {
+//                 System.out.print("("+curr+","+numbers[j]+") ");
+//             }        
+//         System.out.println();
 //         }
 //     }
-
-
 //     public static void main(String[]args) {
 //         int numbers[] = {2,4,6,8,10};
 //         printPairs(numbers);
 //     }
-// }
 
+// }
+// // public class Pairs{
+
+// //     public static void  printPairs(int numbers[]) {
+// //         for(int i =0;i<numbers.length;i++) {
+// //            int curr = numbers[i];
+// //             for(int j = i+1;j<numbers.length;j++) {
+// //                 System.out.println("("+curr+","+ numbers[j]+ ") ");
+
+// //             }
+// //             System.out.println();
+
+// //         }
+// //     }
+
+
+// //     public static void main(String[]args) {
+// //         int numbers[] = {2,4,6,8,10};
+// //         printPairs(numbers);
+// //     }
+// // }
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Pairs {
+
+    // Generic method for any type of array
+    public static <T> void printPairs(T[] arr) {
+        IntStream.range(0, arr.length)
+                 .forEach(i -> IntStream.range(i + 1, arr.length)
+                                         .forEach(j -> System.out.println("(" + arr[i] + "," + arr[j] + ")")));
+    }
+
+    public static void main(String[] args) {
+        Integer[] numbers = {2, 4, 6, 8, 10};
+        printPairs(numbers);
+
+        // You can also test with Strings
+        String[] letters = {"A", "B", "C"};
+        printPairs(letters);
+    }
+}
