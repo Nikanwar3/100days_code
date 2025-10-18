10823137bd870fa848065deaf3389ef630e7998f
Nikanwar3 <nikanwar333@gmail.com>
2025-10-18 22:26:59 +0530

    statement

--- Basics/statement.java
+++ Basics/statement.java
@@ -102,7 +102,7 @@ public class Statement {
         if (num == 2) return true;
         if (num % 2 == 0) return false;
         for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) return false;
+            if (num % i == 0) return false;// statement
         }
         return true;
     }
