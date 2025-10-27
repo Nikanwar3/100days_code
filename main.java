ce53b7a9ed96b217c9dda1c25f9911b15a574b20
Nikanwar3 <nikanwar333@gmail.com>
2025-10-27 22:12:56 +0530

    Added optimized solution for K Smallest Sum Pairs in Java

--- main.java
+++ main.java
@@ -28,4 +28,50 @@
 //             return left.peek();
 //     }
 // }
+import java.util.*;
 
+class Solution {
+    static class Pair {
+        int i, j, sum;
+        Pair(int i, int j, int sum) {
+            this.i = i;
+            this.j = j;
+            this.sum = sum;
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> kSmallestPair(int[] arr1, int[] arr2, int k) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (arr1.length == 0 || arr2.length == 0 || k == 0) return result;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.sum - b.sum);
+        Set<String> visited = new HashSet<>();
+
+        // Push the smallest pair (0, 0)
+        pq.offer(new Pair(0, 0, arr1[0] + arr2[0]));
+        visited.add("0,0");
+
+        while (!pq.isEmpty() && result.size() < k) {
+            Pair curr = pq.poll();
+
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(arr1[curr.i]);
+            temp.add(arr2[curr.j]);
+            result.add(temp);
+
+            // Next pair in arr1 direction
+            if (curr.i + 1 < arr1.length && !visited.contains((curr.i + 1) + "," + curr.j)) {
+                pq.offer(new Pair(curr.i + 1, curr.j, arr1[curr.i + 1] + arr2[curr.j]));
+                visited.add((curr.i + 1) + "," + curr.j);
+            }
+
+            // Next pair in arr2 direction
+            if (curr.j + 1 < arr2.length && !visited.contains(curr.i + "," + (curr.j + 1))) {
+                pq.offer(new Pair(curr.i, curr.j + 1, arr1[curr.i] + arr2[curr.j + 1]));
+                visited.add(curr.i + "," + (curr.j + 1));
+            }
+        }
+
+        return result;
+    }
+}
