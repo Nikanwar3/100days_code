4fa56d845e8fe2c61eee4f558e59a6a9ba767f38
Nikanwar3 <nikanwar333@gmail.com>
2025-11-12 22:45:15 +0530

    array

--- Array.java
+++ Array.java
@@ -49,7 +49,7 @@ import java.util.*;
 public class Array{
     public static void main(String[] args){//
         Scanner sc = new Scanner(System.in);
-        int marks[] = new int[50];
+        int marks[] = new int[50];//
         marks[0] = sc.nextInt();
         marks[1] = sc.nextInt();// input 
         marks[2] = sc.nextInt();
