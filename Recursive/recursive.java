f90a38b6823b367279c2dc035a84d14bc0da06f3
Nikanwar3 <nikanwar333@gmail.com>
2025-12-12 22:28:31 +0530

    recursive

--- Recursive/recursive.java
+++ Recursive/recursive.java
@@ -1,8 +1,16 @@
 package Recursive;
 
 public class recursive {
-    public 
-    public public static void main(String[] args) {
+    public static void recursiveFunction(int n) {
+        if (n <= 0) {
+            System.out.println("Base case reached");
+            return;
+        }
+        System.out.println("Current value: " + n);
+        recursiveFunction(n - 1);
+
+    }
+    public  static void main(String[] args) {
         
     }
 
