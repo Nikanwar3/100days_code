6ab95dc802de3631f68126410637a6bdaad64e8d
Nikanwar3 <nikanwar333@gmail.com>
2025-12-29 20:59:04 +0530

    binary

--- binaryTree/binaryTree.java
+++ binaryTree/binaryTree.java
@@ -1,59 +1,106 @@
 
+// // public class binaryTree{
+
+
+// //     static class Node{
+// //         int data;
+// //         Node left = null;
+// //         Node right = null;
+// //         Node(int data){
+// //             this.data = data;
+// //             this.right = null;
+// //             this.left = null;
+
+// //         }
+
+// //         static class BinaryTree{
+// //              static int index = -1;
+// //              public static Node buildTree(){
+// //         }
+// //     }
+// //         public static void main(String[]args){ 
+// //         int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // 
+// //     }
+// // }
 // public class binaryTree{
 
 
 //     static class Node{
-//         int data;
-//         Node left = null;
-//         Node right = null;
+//         int data;//
+//         Node right;
+//         Node left;
 //         Node(int data){
-//             this.data = data;
-//             this.right = null;
+//             this.data = data;//
+            
+//             this.right = null;//
 //             this.left = null;
-
-//         }
-
-//         static class BinaryTree{
-//              static int index = -1;
-//              public static Node buildTree(){
 //         }
 //     }
-//         public static void main(String[]args){ 
-//         int node [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // 
+//     static class BinaryTree{
+//         static int index = -1;
+//         public static Node buildTree(int nodes[]){
+//             index++;
+//             if(nodes[index] == -1){
+//                 return null;
+//             }
+//             Node newNode = new Node(nodes[index]);
+//             newNode.left = buildTree(nodes);
+//             newNode.right = buildTree(nodes);
+//             return newNode;
+//         }
+//     }
+
+//     public static void main(String[]args){
+//         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; //nodes
+//         //
+//         //
+
 //     }
 // }
-public class binaryTree{
+import java.io.*;
+import java.util.*;
 
+public class binaryTree {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-    static class Node{
-        int data;//
-        Node right;
-        Node left;
-        Node(int data){
-            this.data = data;//
-            
-            this.right = null;//
-            this.left = null;
-        }
-    }
-    static class BinaryTree{
-        static int index = -1;
-        public static Node buildTree(int nodes[]){
-            index++;
-            if(nodes[index] == -1){
-                return null;
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String[] parts = br.readLine().split(" ");
+            long a = Long.parseLong(parts[0]);
+            long b = Long.parseLong(parts[1]);
+
+            int ans = 0;
+            long white1 = 0, dark1 = 0;  // case 1: top white
+            long white2 = 0, dark2 = 0;  // case 2: top dark
+            long cur = 1;
+
+            while (true) {
+                boolean ok = false;
+
+                // case 1
+                if (ans % 2 == 0) white1 += cur;
+                else dark1 += cur;
+
+                // case 2
+                if (ans % 2 == 0) dark2 += cur;
+                else white2 += cur;
+
+                if ((white1 <= a && dark1 <= b) ||
+                    (white2 <= a && dark2 <= b)) {
+                    ok = true;
+                }
+
+                if (!ok) break;
+
+                ans++;
+                cur <<= 1;
             }
-            Node newNode = new Node(nodes[index]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-            return newNode;
+
+            sb.append(ans).append("\n");
         }
-    }
 
-    public static void main(String[]args){
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; //nodes
-        //
-        //
-
+        System.out.print(sb.toString());
     }
 }
