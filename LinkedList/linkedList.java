bd8174c2c83b04757662fbb3b6f19a81be835f2a
Nikanwar3 <nikanwar333@gmail.com>
2025-12-30 21:08:29 +0530

    add no linkedlist

--- LinkedList/linkedList.java
+++ LinkedList/linkedList.java
@@ -1,87 +1,149 @@
-// class Node {
-//     int data;
-//     Node next;
+// // class Node {
+// //     int data;
+// //     Node next;
     
-//     Node(int val) {
-//         data = val;
-//         next = null;
-//     }
-// }
+// //     Node(int val) {
+// //         data = val;
+// //         next = null;
+// //     }
+// // }
 
-// class linkedList {
-//     static Node head;
+// // class linkedList {
+// //     static Node head;
     
-//     // Insert at the beginning
-//     static void insertbegin(int ele) {
-//         Node newnode = new Node(ele);
-//         if (head == null) {
-//             head = newnode;
-//         } else {
-//             newnode.next = head;
-//             head = newnode;
-//         }
-//     }
+// //     // Insert at the beginning
+// //     static void insertbegin(int ele) {
+// //         Node newnode = new Node(ele);
+// //         if (head == null) {
+// //             head = newnode;
+// //         } else {
+// //             newnode.next = head;
+// //             head = newnode;
+// //         }
+// //     }
     
-//     // Insert at the end
-//     static void insertend(int ele) {
-//         Node newnode = new Node(ele);
-//         if (head == null) {
-//             head = newnode;
-//             return;
-//         }
+// //     // Insert at the end
+// //     static void insertend(int ele) {
+// //         Node newnode = new Node(ele);
+// //         if (head == null) {
+// //             head = newnode;
+// //             return;
+// //         }
         
-//         Node temp = head;
-//         while (temp.next != null) {
-//             temp = temp.next;
-//         }
-//         temp.next = newnode;
-//     }
+// //         Node temp = head;
+// //         while (temp.next != null) {
+// //             temp = temp.next;
+// //         }
+// //         temp.next = newnode;
+// //     }
     
-//     // Display the list
-//     static void display() {
-//         if (head == null) {
-//             System.out.println("Linked list is empty");
-//         } else {
-//             Node temp = head;
-//             while (temp != null) {
-//                 System.out.print(temp.data + " ");
-//                 temp = temp.next;
+// //     // Display the list
+// //     static void display() {
+// //         if (head == null) {
+// //             System.out.println("Linked list is empty");
+// //         } else {
+// //             Node temp = head;
+// //             while (temp != null) {
+// //                 System.out.print(temp.data + " ");
+// //                 temp = temp.next;
+// //             }
+// //             System.out.println(); // New line after printing all elements
+// //         }
+// //     }
+    
+// //     public static void main(String[] args) {
+// //         insertbegin(1);
+// //         insertbegin(3);
+// //         insertend(11);
+// //         display();
+// //     }
+// // }
+
+// public class linkedList{
+//     public static class Node{
+//         int data;
+//         Node next;
+
+//         public Node(int data){
+//             this.data = data;
+//             this.next = null;
+
+//             public void addFirst(int data) {// Insert at the beginning
+//                 Node newNode = new Node(data);
+//                 if (head == null) {
+//                     head = newNode;
+//                 } else {
+//                     newNode.next = head;
+//                     head = newNode;
+//                 }
+                
 //             }
-//             System.out.println(); // New line after printing all elements
 //         }
 //     }
-    
-//     public static void main(String[] args) {
-//         insertbegin(1);
-//         insertbegin(3);
-//         insertend(11);
-//         display();
+//     public static void main(String[]args){
+//         linkedList ll = new linkedList();//linkedList object
+
 //     }
 // }
+/*
+class Node {
+    int data;
+    Node next;
 
-public class linkedList{
-    public static class Node{
-        int data;
-        Node next;
-
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-
-            public void addFirst(int data) {// Insert at the beginning
-                Node newNode = new Node(data);
-                if (head == null) {
-                    head = newNode;
-                } else {
-                    newNode.next = head;
-                    head = newNode;
-                }
-                
-            }
-        }
+    Node(int d) {
+        data = d;
+        next = null;
     }
-    public static void main(String[]args){
-        linkedList ll = new linkedList();//linkedList object
+}
+*/
 
+class Solution {
+    
+    private Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    public Node addTwoLists(Node head1, Node head2) {
+        // Reverse both lists
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+        
+        Node dummy = new Node(0);
+        Node tail = dummy;
+        int carry = 0;
+        
+        // Add digits
+        while (head1 != null || head2 != null || carry != 0) {
+            int sum = carry;
+            if (head1 != null) {
+                sum += head1.data;
+                head1 = head1.next;
+            }
+            if (head2 != null) {
+                sum += head2.data;
+                head2 = head2.next;
+            }
+            
+            carry = sum / 10;
+            tail.next = new Node(sum % 10);
+            tail = tail.next;
+        }
+        
+        // Reverse result to get MSD first
+        Node result = reverse(dummy.next);
+        
+        // Remove leading zeros (but keep one zero if all zeros)
+        while (result != null && result.data == 0 && result.next != null) {
+            result = result.next;
+        }
+        
+        return result;
     }
 }
