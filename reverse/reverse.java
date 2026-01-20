08f88e46879a362c62a9eb899f37297da8df8307
Nikanwar3 <nikanwar333@gmail.com>
2026-01-20 21:56:27 +0530

    reverse

--- reverse/reverse.java
+++ reverse/reverse.java
@@ -109,7 +109,7 @@ public class reverse {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         int numbers[] = {2, 4, 6, 8, 10};
         reverse(numbers);//
         for (int i = 0; i < numbers.length; i++) {
