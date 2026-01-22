23f98338205a8f2c46a828f5daeae69173ad9bac
Nikanwar3 <nikanwar333@gmail.com>
2026-01-22 22:32:31 +0530

    string

--- String/string.java
+++ String/string.java
@@ -5,7 +5,7 @@ public class string{
         for(int i =0; i<str.length();i++){
             System.out.print(str.charAt(i) + "");
         }
-        System.out.println();
+        System.out.println();//print a new line after printing all letters
     }
 
     public static void main(String[]args){//
