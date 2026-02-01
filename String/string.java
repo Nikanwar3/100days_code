6c2cee02ea043dc3012490012c89d63c42cced8f
Nikanwar3 <nikanwar333@gmail.com>
2026-02-01 20:34:51 +0530

    string

--- String/string.java
+++ String/string.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class string{
-    public static void printLetters(String str){
-        for(int i =0; i<str.length();i++){
+    public static void printLetters(String str){//
+        for(int i =0; i<str.length();i++){//
             System.out.print(str.charAt(i) + "");//
         }
         System.out.println();//print a new line after printing all letters
