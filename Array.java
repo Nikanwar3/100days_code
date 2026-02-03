4815b8d50ca7bb66b7b31f85dc880027a80355d0
Nikanwar3 <nikanwar333@gmail.com>
2026-02-03 22:26:56 +0530

    array

--- Array.java
+++ Array.java
@@ -166,7 +166,7 @@ import java.util.Scanner;
 
 class Codechef//
 {
-	public static void main (String[] args)
+	public static void main (String[] args)//
 	{
 	    // Do not print anything here before taking input
 	    Scanner sc = new Scanner(System.in);
