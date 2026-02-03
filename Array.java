ffa8dde9882dcd75608eaa15300184685b29290c
Nikanwar3 <nikanwar333@gmail.com>
2026-02-03 22:28:47 +0530

    array

--- Array.java
+++ Array.java
@@ -169,7 +169,7 @@ class Codechef//
 	public static void main (String[] args)//
 	{
 	    // Do not print anything here before taking input
-	    Scanner sc = new Scanner(System.in);
+	    Scanner sc = new Scanner(System.in);//
 		int n = sc.nextInt();
 		
 		// Print the value of n
