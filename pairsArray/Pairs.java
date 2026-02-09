660089a8dbf34f9d05a3c432bfd03dac33084885
Nikanwar3 <nikanwar333@gmail.com>
2026-02-09 21:35:45 +0530

    pairs

--- pairsArray/Pairs.java
+++ pairsArray/Pairs.java
@@ -63,8 +63,8 @@ public class Pairs{
 	public static void printPairs(int number[]){
 		for(int i =0;i<number.length;i++){
 			int curr = number[i];
-		for(int j = i+1;j<number.length;j++){
-			System.out.print("("+ curr + "," + number[j] + ")");
+		for(int j = i+1;j<number.length;j++){//
+			System.out.print("("+ curr + "," + number[j] + ")");//
 }
 			System.out.println();//
 }
