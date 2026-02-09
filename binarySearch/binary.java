f69c0d38ece8a8e0096d97664320500535ca5cbf
Nikanwar3 <nikanwar333@gmail.com>
2026-02-09 21:37:25 +0530

    binary Traversal

--- binarySearch/binary.java
+++ binarySearch/binary.java
@@ -1,88 +1,104 @@
-// // public class binarySearch {
-// //     public static int binarySearch(int numbers[], int key) {
-// //         int start = 0, end = numbers.length - 1;
+// // // public class binarySearch {
+// // //     public static int binarySearch(int numbers[], int key) {
+// // //         int start = 0, end = numbers.length - 1;
 
-// //         while (start <= end) {
-// //             int mid = (start + end) / 2;
+// // //         while (start <= end) {
+// // //             int mid = (start + end) / 2;
             
-// //             if (numbers[mid] == key) {
+// // //             if (numbers[mid] == key) {
+// // //                 return mid;
+// // //             } else if (numbers[mid] < key) {
+// // //                 start = mid + 1; // Search right half
+// // //             } else {
+// // //                 end = mid - 1;   // Search left half
+// // //             }
+// // //         }
+        
+// // //         return -1; // Key not found
+// // //     }
+
+// // //     public static void main(String[] args) {
+// // //         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+// // //         int key = 10;
+
+// // //         int result = binarySearch(numbers, key);
+// // //         if (result != -1) {
+// // //             System.out.println("Element found at index: " + result);
+// // //         } else {
+// // //             System.out.println("Element not found"); // Output if key is not found
+// // //         }
+// // //     }
+// // // }
+
+// // public class binarySearch{
+// //     public  static int binary(int numbers[],int key){
+// //         int start = 0, end = numbers.length -1 ;
+// //         while(start<=end){
+// //             int mid = (start + end)/2;
+// //             if(numbers[mid]== key){
 // //                 return mid;
-// //             } else if (numbers[mid] < key) {
-// //                 start = mid + 1; // Search right half
-// //             } else {
-// //                 end = mid - 1;   // Search left half
 // //             }
-// //         }
+// //             else if (numbers[mid] < key){//right
+// //                 start = mid + 1;
+// //             }
+// //             else{
+// //                 end = mid - 1;
+// //             }
+
+// //             }
+// //             return -1;
+
+// //             }
         
-// //         return -1; // Key not found
-// //     }
-
-// //     public static void main(String[] args) {
-// //         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
-// //         int key = 10;
-
-// //         int result = binarySearch(numbers, key);
-// //         if (result != -1) {
-// //             System.out.println("Element found at index: " + result);
-// //         } else {
-// //             System.out.println("Element not found"); // Output if key is not found
+// //         public static void main(String[]args){
+// //             int numbers[] = {2,4,6,8,10};
+// //             int key = 6;
 // //         }
-// //     }
+// //         System.out.println(binary(numbers,key));
 // // }
-
-// public class binarySearch{
-//     public  static int binary(int numbers[],int key){
-//         int start = 0, end = numbers.length -1 ;
-//         while(start<=end){
-//             int mid = (start + end)/2;
-//             if(numbers[mid]== key){
-//                 return mid;
-//             }
-//             else if (numbers[mid] < key){//right
-//                 start = mid + 1;
-//             }
-//             else{
-//                 end = mid - 1;
-//             }
-
-//             }
-//             return -1;
-
-//             }
-        
-//         public static void main(String[]args){
-//             int numbers[] = {2,4,6,8,10};
-//             int key = 6;
-//         }
-//         System.out.println(binary(numbers,key));
-// }
-// }
+// // }
     
 
     
-import java.util.*;
-public class binary{
-	public static int binarySearch(int numbers[],int key){
-		int start =0, end = numbers.length-1;//
-		while(start<=end){
-		int mid = (start + end)/2;//
-		if(numbers[mid] == key){
-			return mid;
-		}
-		if(numbers[mid]<key){
-			start = mid + 1;}
-else{
-	end = mid -1;//
-}
-}
-return -1;//
-	}
+// import java.util.*;
+// public class binary{
+// 	public static int binarySearch(int numbers[],int key){
+// 		int start =0, end = numbers.length-1;//
+// 		while(start<=end){
+// 		int mid = (start + end)/2;//
+// 		if(numbers[mid] == key){
+// 			return mid;
+// 		}
+// 		if(numbers[mid]<key){
+// 			start = mid + 1;}
+// else{
+// 	end = mid -1;//
+// }
+// }
+// return -1;//
+// 	}
 		
 
 
-	public static void main(String[]args){
-		int numbers[] = {2,4,6,8,10};
-		int key = 10;
-		System.out.println("key at :" + binarySearch(numbers,key));
-	}
+// 	public static void main(String[]args){
+// 		int numbers[] = {2,4,6,8,10};
+// 		int key = 10;
+// 		System.out.println("key at :" + binarySearch(numbers,key));
+// 	}
+// }
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        return result;
+    }
+    
+    private void preorder(TreeNode node, List<Integer> result) {
+        if(node == null) return;
+        
+        result.add(node.val);      // Root
+        preorder(node.left, result);  // Left
+        preorder(node.right, result); // Right
+    }
 }
