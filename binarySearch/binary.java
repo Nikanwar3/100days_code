ce8a5380df017430785914e6e667997a6da7cbee
Nikanwar3 <nikanwar333@gmail.com>
2026-02-10 21:11:19 +0530

    binary

--- binarySearch/binary.java
+++ binarySearch/binary.java
@@ -87,15 +87,15 @@
 // 	}
 // }
 
-class Solution {
+class Solution {//
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preorder(root, result);
+        List<Integer> result = new ArrayList<>();//
+        preorder(root, result);//
         return result;
     }
     
     private void preorder(TreeNode node, List<Integer> result) {
-        if(node == null) return;
+        if(node == null) return;//
         
         result.add(node.val);      // Root
         preorder(node.left, result);  // Left
