bce0dba555e4c80edfb082cd5b161d107972f5c9
Nikanwar3 <nikanwar333@gmail.com>
2026-02-22 21:06:48 +0530

    binarytree

--- binarySearch/binary.java
+++ binarySearch/binary.java
@@ -88,10 +88,10 @@
 // }
 
 class Solution {//
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {//
         List<Integer> result = new ArrayList<>();//
         preorder(root, result);//
-        return result;
+        return result;//
     }
     
     private void preorder(TreeNode node, List<Integer> result) {//
