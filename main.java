4ced4352709ddb6acf322526ec6665940daa386e
Nikanwar3 <nikanwar333@gmail.com>
2026-05-12 22:05:51 +0530

    recursive

--- main.java
+++ main.java
@@ -1,54 +1,54 @@
-// Reverse a Linked List - LeetCode #206
-// Given the head of a singly linked list, reverse the list and return the reversed list.
+// // Reverse a Linked List - LeetCode #206
+// // Given the head of a singly linked list, reverse the list and return the reversed list.
 
-class main {
-    static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int val) {
-            this.val = val;
-            this.next = null;
-        }
-    }
+// class main {
+//     static class ListNode {
+//         int val;
+//         ListNode next;
+//         ListNode(int val) {
+//             this.val = val;
+//             this.next = null;
+//         }
+//     }
 
-    public static ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
+//     public static ListNode reverseList(ListNode head) {
+//         ListNode prev = null;
+//         ListNode curr = head;
 
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
+//         while (curr != null) {
+//             ListNode next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
 
-        return prev;
-    }
+//         return prev;
+//     }
 
-    public static void printList(ListNode head) {
-        ListNode curr = head;
-        while (curr != null) {
-            System.out.print(curr.val);
-            if (curr.next != null) System.out.print(" -> ");
-            curr = curr.next;
-        }
-        System.out.println();
-    }
+//     public static void printList(ListNode head) {
+//         ListNode curr = head;
+//         while (curr != null) {
+//             System.out.print(curr.val);
+//             if (curr.next != null) System.out.print(" -> ");
+//             curr = curr.next;
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String[] args) {
-        // Create list: 1 -> 2 -> 3 -> 4 -> 5
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+//     public static void main(String[] args) {
+//         // Create list: 1 -> 2 -> 3 -> 4 -> 5
+//         ListNode head = new ListNode(1);
+//         head.next = new ListNode(2);
+//         head.next.next = new ListNode(3);
+//         head.next.next.next = new ListNode(4);
+//         head.next.next.next.next = new ListNode(5);
 
-        System.out.print("Original: ");
-        printList(head);
+//         System.out.print("Original: ");
+//         printList(head);
 
-        head = reverseList(head);
+//         head = reverseList(head);
 
-        System.out.print("Reversed: ");
-        printList(head);
-    }
-}
+//         System.out.print("Reversed: ");
+//         printList(head);
+//     }
+// }
